a81fd8375e6f185b9d6e744184fdfa3dfdb31680
Alexandre Petibon <alexandre.petibon@outlook.fr>
2023-04-19 14:46:50 +0200

    avancement étape 1.2 + mise en place du login

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -13,5 +13,57 @@ fetch("http://localhost:5678/api/works")
       projectElement.appendChild(titleElement);
       gallery.appendChild(projectElement);
     });
+    console.log(data)
   })
   .catch(error => console.error(error));
+
+  
+  const buttonTous = document.querySelector('#buttonG');
+  const buttonT = document.createElement('button');
+  buttonT.innerText = 'Tous';
+  buttonT.setAttribute('id', 'button');
+  buttonT.addEventListener('click', () => {
+    console.log('Vous avez cliqué sur le bouton');
+  });
+  buttonTous.appendChild(buttonT);
+  
+
+  const buttonNames = ['Objets', 'Appartements', 'Hôtels & Restaurants'];
+  const buttonIDs = ['buttonO', 'buttonA', 'buttonH'];
+  const categoryIDs = [1, 2, 3];
+  const buttonParent = document.querySelector('#buttonG');
+  
+  for (let i = 0; i < buttonNames.length; i++) {
+    const button = document.createElement('button');
+    button.innerText = buttonNames[i];
+    button.setAttribute('id', 'button', buttonIDs[i]);
+    button.addEventListener('click', () => {
+      if (categoryIDs[i] === 1) {
+        fetch(`http://localhost:5678/api/categories`)
+          .then(response => response.json())
+          .then(data => {
+            const filterData = data.filter(category => category.id === 1);
+            console.log(filterData);
+          })
+          .catch(error => console.error(error));
+      } else if (categoryIDs[i] === 2) {
+        fetch(`http://localhost:5678/api/categories`)
+          .then(response => response.json())
+          .then(data => {
+            const filterData = data.filter(category => category.id === 2);
+            console.log(filterData);
+          })
+          .catch(error => console.error(error));
+      } else if (categoryIDs[i] === 3) {
+        fetch(`http://localhost:5678/api/categories`)
+          .then(response => response.json())
+          .then(data => {
+            const filterData = data.filter(category => category.id === 3);
+            console.log(filterData);
+          })
+          .catch(error => console.error(error));
+      }
+      console.log('Vous avez cliqué sur le bouton');
+    });
+    buttonParent.appendChild(button);
+  }
---
+++ FrontEnd/login.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('#envoyer');
+console.log (form)
+form.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#pass').value;
+  console.log (email)
+  console.log(password)
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ email, password })
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    else {
+      throw new Error('Mauvais e-mail et/ou mot de passe');
+    }
+  })
+  .then(data => {
+    localStorage.setItem('token', data.token);
+    window.location.href="index.html"
+  })
+  .catch(error => {
+    alert(error.message);
+    console.error(error);
+  });
+});
